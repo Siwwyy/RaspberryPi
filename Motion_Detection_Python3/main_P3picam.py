919d4159ac898ca58ee16b04e2bd9ccbe440f4e5
Siwy <andrydamian@gmail.com>
2020-06-01 19:53:17 +0200

    working real time detecting A or B in Sign Alphabet

--- Motion_Detection_Python3/main_P3picam.py
+++ Motion_Detection_Python3/main_P3picam.py
@@ -2,6 +2,9 @@ import P3picam_Python3
 import picamera
 from datetime import datetime
 from subprocess import call
+import Testing_Trained_Model as Test
+import time
+import cv2
 
 
 motion_state = False
@@ -38,17 +41,33 @@ def Time_Stamp(current_time, picture_path, picture_name):
     
     
     
-
+print("Ready to detecting")
 
 while True:
+    key = cv2.waitKey(0)
+    if key == 27: # escape
+        break
     motion_state = P3picam_Python3.motion()
     #print(motion_state)
     if motion_state == True:
         print("Motion detected")
         #current_time = Get_Time()
-        picture_name = Capture_Image(current_time, file_path)
-        
+        #picture_name = Capture_Image(current_time, file_path)
+        picture_name = ""
+        with picamera.PiCamera() as camera:
+            picture_name = "CAPTURED/motion"+str(motion_counter)+".jpg"
+            camera.resolution = (1280,720)
+            camera.capture(picture_name)
+            motion_counter += 1
         #PASTE NEURAL NETWORK HERE
+        print(picture_name)
+        predictions = Test.model.predict([Test.Prepare(picture_name)])
+        print("You are showing following sign: ",Test.Class_names[int(predictions[0][0])])
+        print("Sleeping... \n")
+        time.sleep(1)
+        
+        
+        
         
         #Time_Stamp(current_time,file_path,picture_name)
 #        with picamera.PiCamera() as camera:
@@ -56,5 +75,5 @@ while True:
 #            camera.resolution = (1280,720)
 #            camera.capture(motion_file_path)
 #            motion_counter += 1
-            
- 
+    
+#Test.Predict()
